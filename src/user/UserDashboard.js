373c7fb934e339fc7e71681f8bc7e2700fe28c2a
Nguyen Ho Hai Phuoc <nhhphuoc1993@gmail.com>
2020-05-23 15:39:11 +0700

    update UserDashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -30,7 +30,7 @@ const UserDashboard = () => {
     const userLinks = () => {
         return (
             <div className="card">
-                <h4 className="card-header">User Links</h4>
+                <h4 className="card-header">Navigation</h4>
                 <ul className="list-group">
                     <li className="list-group-item">
                         <Link className="nav-link" to="/cart">
@@ -49,12 +49,21 @@ const UserDashboard = () => {
 
     const userInfo = () => {
         return (
-            <div className="card mb-5">
-                <h3 className="card-header">User Information</h3>
+            <div className="card mb-4">
+                <h3 className="card-header">Profile</h3>
                 <ul className="list-group">
-                    <li className="list-group-item">{name}</li>
-                    <li className="list-group-item">{email}</li>
-                    <li className="list-group-item">{role === 1 ? "Admin" : "Registered User"}</li>
+                    <li className="list-group-item">
+                        <strong>Name: </strong>
+                        {name}
+                    </li>
+                    <li className="list-group-item">
+                        <strong>Email: </strong>
+                        {email}
+                    </li>
+                    <li className="list-group-item">
+                        <strong>Account type: </strong>
+                        {role === 1 ? "Admin" : "Normal user"}
+                    </li>
                 </ul>
             </div>
         );
@@ -62,25 +71,90 @@ const UserDashboard = () => {
 
     const purchaseHistory = (history) => {
         return (
-            <div className="card mb-5">
-                <h3 className="card-header">Purchase history</h3>
+            <div className="card mb-4">
+                <h3 className="card-header">Purchased history</h3>
                 <ul className="list-group">
                     <li className="list-group-item">
                         {history.map((h, i) => {
                             return (
                                 <div key={i}>
-                                    <hr />
-                                    {h.products.map((p, i) => {
-                                        return (
-                                            <div key={i}>
-                                                <h6>Product name: {p.name}</h6>
-                                                <h6>Product price: ${p.price}</h6>
-                                                <h6>
-                                                    Purchased date: {moment(p.createdAt).fromNow()}
-                                                </h6>
+                                    {i > 0 && <hr />}
+                                    <h6>
+                                        <strong>Order ID: </strong>
+                                        {h._id}
+                                    </h6>
+                                    <h6>
+                                        <strong>Purchased date: </strong>
+                                        {moment(h.createdAt).fromNow()}
+                                    </h6>
+                                    <h6>
+                                        <strong>Status: </strong>
+                                        {h.status}
+                                    </h6>
+                                    <h6>
+                                        <strong>Total price: </strong>${h.amount}
+                                    </h6>
+                                    <h6>
+                                        <strong>Delivery address: </strong>
+                                        {h.address ? h.address : h.address}
+                                    </h6>
+                                    <div class="accordion" id={"puchasedItemsAccordion_" + i}>
+                                        <div class="card z-depth-0 bordered">
+                                            <div
+                                                class="card-header cloudy-knoxville-gradient"
+                                                id={"puchasedItemsAccordion_" + i + "_headingOne"}
+                                            >
+                                                <h5 class="mb-0">
+                                                    <button
+                                                        class="btn btn-link btn-block text-left p-0"
+                                                        type="button"
+                                                        data-toggle="collapse"
+                                                        data-target={
+                                                            "#puchasedItemsAccordion_" +
+                                                            i +
+                                                            "_collapseOne"
+                                                        }
+                                                        aria-expanded="true"
+                                                        aria-controls="collapseOne"
+                                                    >
+                                                        Purchased items
+                                                    </button>
+                                                </h5>
                                             </div>
-                                        );
-                                    })}
+                                            <div
+                                                id={"puchasedItemsAccordion_" + i + "_collapseOne"}
+                                                class="collapse show"
+                                                aria-labelledby={
+                                                    "puchasedItemsAccordion_" + i + "_headingOne"
+                                                }
+                                                data-parent={"#puchasedItemsAccordion_" + i}
+                                            >
+                                                <div class="card-body">
+                                                    {h.products.map((p, i) => {
+                                                        return (
+                                                            <div key={i}>
+                                                                {i > 0 && <hr />}
+                                                                <h6>
+                                                                    <strong>Name: </strong>
+                                                                    {p.name}
+                                                                </h6>
+                                                                <h6>
+                                                                    <strong>Single price: </strong>$
+                                                                    {p.price}
+                                                                </h6>
+                                                                <h6>
+                                                                    <strong>
+                                                                        Bought quantity:{" "}
+                                                                    </strong>
+                                                                    ${p.count}
+                                                                </h6>
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             );
                         })}
@@ -91,7 +165,11 @@ const UserDashboard = () => {
     };
 
     return (
-        <Layout title="User Dashboard" description={`G'day ${name}!`} className="container-fluid">
+        <Layout
+            title="User Dashboard"
+            description={`G'day ${name}!`}
+            className="container-fluid mt-4"
+        >
             <div className="row">
                 <div className="col-3">{userLinks()}</div>
                 <div className="col-9">
